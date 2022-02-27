80720a4feaa5cac775b6297e38e0781649c674f0
TobiasMeyerhoefer <97233541+TobiasMeyerhoefer@users.noreply.github.com>
2022-02-27 23:03:18 +0100

    Add files via upload

--- Convergence.R
+++ Convergence.R
@@ -10,34 +10,35 @@ library(dplyr)
 library(mi)
 library(miceadds)
 
-
+#Reproduzierbarkeit
 set.seed(1)
-R_mean_10 = list(len=100)
+#Listen für Schätzer
 R_mean_30 = list(len=100)
-R_mean_50 = list(len=100)
-R_mean_80 = list(len=100)
-
-
-R_var_10 = list(len=100)
+R_mean_70 = list(len=100)
+R_Mean_my = list(len=100)
 R_var_30 = list(len=100)
-R_var_50 = list(len=100)
-R_var_80 = list(len=100)
+R_var_70 = list(len=100)
+R_var_my = list(len=100)
+R_Rsq_30 = list(len=100)
+R_Rsq_70 = list(len=100)
+R_Rsq_my = list(len=100)
+
 # Data generation
 
 for (i in 1:20){
   
   n <- 1000
   number_variables <- 6
-  mu <- runif(number_variables, -5, 10) 
-  v <- runif(number_variables, 0, 15)
+  mu <- runif(number_variables, -5, 10)
+  v <- rWishart(6, 6, matrix(0.5, number_variables, number_variables)) # not sure what to do here
   
   y <- rnorm(n, mu[1], v[1])
-  x1 <- 18 - 0.7 * y + rnorm(n, mu[2], v[2])
-  x2 <- 5 + 0.5 * y + 0.7 * x1 + rnorm(n, mu[3], v[3])
-  x3 <- 1 - 0.3 * y - 1 * x1 + 0.2 * x2 + rnorm(n, mu[4], v[4])
-  x4 <-13 + 0.9 * y + 0.4 * x1 - 0.6 * x2 - 0.5 * x3 + rnorm(n, mu[5], v[5])
-  x5 <- 6 + 0.1 * y - 1.3 * x1 + 0.75 * x2 - 0.2 * x3 + 0.8 * x4 + rnorm(n, mu[6], v[6])
-  data_orig <- data.frame(y, x1, x2, x3, x4, x5)
+  X1 <- 18 - 0.7 * y + rnorm(n, mu[2], v[2])
+  X2 <- 5 + 0.5 * y + 0.7 * X1 + rnorm(n, mu[3], v[3])
+  X3 <- 1 - 0.3 * y - 1 * X1 + 0.2 * X2 + rnorm(n, mu[4], v[4])
+  X4 <-13 + 0.9 * y + 0.4 * X1 - 0.6 * X2 - 0.5 * X3 + rnorm(n, mu[5], v[5])
+  X5 <- 6 + 0.1 * y - 1.3 * X1 + 0.75 * X2 - 0.2 * X3 + 0.8 * X4 + rnorm(n, mu[6], v[6])
+  data_orig <- data.frame(y, X1, X2, X3, X4, X5)
   
   ## original values
   (means_orig <- apply(data_orig, 2, FUN = mean))
@@ -59,10 +60,10 @@ for (i in 1:20){
     mcar_values5 <- sample(nrow(data_orig), round(percent_miss * n))
     
     is.na(data_mcar$y[mcar_values1]) <- T
-    is.na(data_mcar$x1[mcar_values2]) <- T
-    is.na(data_mcar$x2[mcar_values3]) <- T
-    is.na(data_mcar$x3[mcar_values4]) <- T
-    is.na(data_mcar$x4[mcar_values5]) <- T
+    is.na(data_mcar$X1[mcar_values2]) <- T
+    is.na(data_mcar$X2[mcar_values3]) <- T
+    is.na(data_mcar$X3[mcar_values4]) <- T
+    is.na(data_mcar$X4[mcar_values5]) <- T
     #x5 bleibt vollständig
     ## graphics of missing pattern
     
@@ -82,28 +83,28 @@ for (i in 1:20){
     
     #remove the lowest quantile
     data_mar <- data_orig
-    z_miss_mar_px <- 0.5 + 2 * x1 - 0.7 * x2 + 0.4 * x3 - 0.8 * x4 + 0.1 * x5 + rnorm(n, 0, 3)
-    mis_mar_px <- z_miss_mar_px < quantile(z_miss_mar_px, p_miss)
+    z_miss_mar_py <- 0.5 + 2 * X1 - 0.7 * X2 + 0.4 * X3 - 0.8 * X4 + 0.1 * X5 + rnorm(n, 0, 3)
+    mis_mar_py <- z_miss_mar_py < quantile(z_miss_mar_py, p_miss)
     
     #für alle Variablen außer x5 durchführen
-    z_miss_mar_px1 <- 0.3 + 2.5 * y - 0.5 * x2 + 0.6 * x3 - 0.9 * x4 + 0.2 * x5 + rnorm(n, 0, 3)
-    mis_mar_px1 <- z_miss_mar_px1 < quantile(z_miss_mar_px1, p_miss)
+    z_miss_mar_pX1 <- 0.3 + 2.5 * y - 0.5 * X2 + 0.6 * X3 - 0.9 * X4 + 0.2 * X5 + rnorm(n, 0, 3)
+    mis_mar_pX1 <- z_miss_mar_pX1 < quantile(z_miss_mar_pX1, p_miss)
 
-    z_miss_mar_px2 <- 1.5 + 1.2 * y - 0.5 * x1 + 0.7 * x3 - 0.2 * x4 + 0.7 * x5 + rnorm(n, 0, 3)
-    mis_mar_px2 <- z_miss_mar_px2 < quantile(z_miss_mar_px2, p_miss)
+    z_miss_mar_pX2 <- 1.5 + 1.2 * y - 0.5 * X1 + 0.7 * X3 - 0.2 * X4 + 0.7 * X5 + rnorm(n, 0, 3)
+    mis_mar_pX2 <- z_miss_mar_pX2 < quantile(z_miss_mar_pX2, p_miss)
     
-    z_miss_mar_px3 <- 0.7 + 0.2 * y - 0.8 * x1 + 0.3 * x2 + 0.1 * x4 + 0.5 * x5 + rnorm(n, 0, 3)
-    mis_mar_px3 <- z_miss_mar_px3 < quantile(z_miss_mar_px3, p_miss)
+    z_miss_mar_pX3 <- 0.7 + 0.2 * y - 0.8 * X1 + 0.3 * X2 + 0.1 * X4 + 0.5 * X5 + rnorm(n, 0, 3)
+    mis_mar_pX3 <- z_miss_mar_pX3 < quantile(z_miss_mar_pX3, p_miss)
     
-    z_miss_mar_px4 <- 0.4 + 0.9 * y + 0.6 * x1 + 0.7 * x2 - 0.9 * x3 + 0.3 * x5 + rnorm(n, 0, 3)
-    mis_mar_px4 <- z_miss_mar_px4 < quantile(z_miss_mar_px4, p_miss)
+    z_miss_mar_pX4 <- 0.4 + 0.9 * y + 0.6 * X1 + 0.7 * X2 - 0.9 * X3 + 0.3 * X5 + rnorm(n, 0, 3)
+    mis_mar_pX4 <- z_miss_mar_pX4 < quantile(z_miss_mar_pX4, p_miss)
     
     #NA setzen der Beobachtungen
-    data_mar$y[mis_mar_px] <- NA
-    data_mar$x1[mis_mar_px1] <- NA
-    data_mar$x2[mis_mar_px2] <- NA
-    data_mar$x3[mis_mar_px3] <- NA
-    data_mar$x4[mis_mar_px4] <- NA
+    data_mar$y[mis_mar_py] <- NA
+    data_mar$X1[mis_mar_pX1] <- NA
+    data_mar$X2[mis_mar_pX2] <- NA
+    data_mar$X3[mis_mar_pX3] <- NA
+    data_mar$X4[mis_mar_pX4] <- NA
     #Muster mal ansehen
     #Anteile überprüfen, ob 0.3 hinkommt.
     summary(data_mar$y)
@@ -114,12 +115,13 @@ for (i in 1:20){
     #Da die Daten auf Zufallszügen basieren, ist hier kein richtiges Sampling notwendig
     #10% der Daten als CC
     #Gleiche große Anzahl an Missings in den Variablen, nicht realisisch aber ist es problematisch?
-    #Funktioniert das so?! Ansonsten wie in Zeilen 102-106 anpassen
+    #Funktioniert das so? Ansonsten wie in Zeilen 102-106 anpassen
     is.na(data_my$y[1:180]) <- T
-    is.na(data_my$x1[181:360]) <- T
-    is.na(data_my$x2[361:540]) <- T
-    is.na(data_my$x3[541:720]) <- T
-    is.na(data_my$x4[721:900]) <- T
+    is.na(data_my$X1[181:360]) <- T
+    is.na(data_my$X2[361:540]) <- T
+    is.na(data_my$X3[541:720]) <- T
+    is.na(data_my$X4[721:900]) <- T
+    
     
     # Imputation
     
@@ -128,10 +130,11 @@ for (i in 1:20){
     mice_mcar <- mice(data_mcar, maxit=5)
     summary(mice_mcar)
     
-    means_mcar <- mice_mcar %>% complete() %>% colMeans()
-    vars_mcar <- apply(mice_mcar %>% complete(), 2, FUN = var)
-    cov_mcar <- mice_mcar %>% complete() %>% cov()
-    cor_mcar <- mice_mcar %>% complete() %>% cor(., use = "pairwise.complete.obs")
+    #Funktioniert nicht
+    #means_mcar <- mice_mcar %>% complete() %>% colMeans()
+    #vars_mcar <- apply(mice_mcar %>% complete(), 2, FUN = var)
+    #cov_mcar <- mice_mcar %>% complete() %>% cov()
+    #cor_mcar <- mice_mcar %>% complete() %>% cor(., use = "pairwise.complete.obs")
     
     # This returns the difference of these values. As there are multiple numbers,
     # I only return the highest, cause it is the most problematic (or is it? 
@@ -145,22 +148,22 @@ for (i in 1:20){
     mice_mar <- mice(data_mar, maxit=5)
     summary(mice_mar)
     
-    means_mar <- mice_mar %>% complete() %>% colMeans()
-    vars_mar <- apply(mice_mar %>% complete(), 2, FUN = var)
-    cov_mar <- mice_mar %>% complete() %>% cov()
-    cor_mar <- mice_mar %>% complete() %>% cor(., use = "pairwise.complete.obs")
+    #means_mar <- mice_mar %>% complete() %>% colMeans()
+    #vars_mar <- apply(mice_mar %>% complete(), 2, FUN = var)
+    #cov_mar <- mice_mar %>% complete() %>% cov()
+    #cor_mar <- mice_mar %>% complete() %>% cor(., use = "pairwise.complete.obs")
     
     #mice "my"
     mice_my <- mice(data_my, maxit=5)
     summary(mice_my)
     
-    means_my <- mice_my %>% complete() %>% colMeans()
-    vars_my <- apply(mice_my %>% complete(), 2, FUN = var)
-    cov_my <- mice_my %>% complete() %>% cov()
-    cor_my <- mice_my %>% complete() %>% cor(., use = "pairwise.complete.obs")
+    #means_my <- mice_my %>% complete() %>% colMeans()
+    #vars_my <- apply(mice_my %>% complete(), 2, FUN = var)
+    #cov_my <- mice_my %>% complete() %>% cov()
+    #cor_my <- mice_my %>% complete() %>% cor(., use = "pairwise.complete.obs")
     
     # Rhat
-    
+    #aktuell werden alle Variablen übertragen, soll das so oder macht das nicht gerade die Probleme?
     if (p_miss == 0.3){
       R_mean_30[i] = Rhat.mice(mice_mcar)[3]
       R_var_30[i] = Rhat.mice(mice_mcar)[4]
@@ -169,32 +172,38 @@ for (i in 1:20){
       R_mean_70[i] = Rhat.mice(mice_mcar)[3]
       R_var_70[i] = Rhat.mice(mice_mcar)[4]
     }
-    #mit mice_my und mice_mar hinzufügen
+    if (p_miss == 0.3){
+      R_mean_30[i] = Rhat.mice(mice_mar)[3]
+      R_var_30[i] = Rhat.mice(mice_mar)[4]
+    }
+    if (p_miss == 0.7){
+      R_mean_70[i] = Rhat.mice(mice_mar)[3]
+      R_var_70[i] = Rhat.mice(mice_mar)[4]
+    }
     
-    
-    # R_mean[i] = Rhat.mice(mice_mcar)[3]
-    #  R_var[i] = Rhat.mice(mice_mcar)[4]
   }
+  R_mean[i] = Rhat.mice(mice_my)[3]
+  R_var[i] = Rhat.mice(mice_my)[4]
 }
 
-# how many means have converged (3 variables x 100 iterations) 
+# how many means have converged (6 variables x 100 iterations) 
 # i have changed the number of iterations for computation time so it isnt at 100 yet
-lapply(R_mean_30,function(x) x[which(x>1.1)]) %>%  unlist() %>% length()
-lapply(R_mean_70,function(x) x[which(x>1.1)]) %>%  unlist() %>% length()
+lapply(R_mean_30,function(x) x[which(x<1.1)]) %>%  unlist() %>% length()
+lapply(R_mean_70,function(x) x[which(x<1.1)]) %>%  unlist() %>% length()
 
-# how many variances have converges (3 variables x 100 iterations)
-lapply(R_var_30,function(x) x[which(x>1.1)]) %>%  unlist() %>% length()
-lapply(R_var_70,function(x) x[which(x>1.1)]) %>%  unlist() %>% length()
+# how many variances have converges (6 variables x 100 iterations)
+lapply(R_var_30,function(x) x[which(x<1.1)]) %>%  unlist() %>% length()
+lapply(R_var_70,function(x) x[which(x<1.1)]) %>%  unlist() %>% length()
 
-#show the values that are above 1.1
-print(as.data.frame(t(lapply(R_mean_50,function(x) x[which(x>1.1)]))))
+#show the values that are under 1.1
+print(as.data.frame(t(lapply(R_mean_30,function(x) x[which(x<1.1)]))))
 
 
 ###MNAR ist nicht relevant, oder? Kann somit entfernt werden?! Sowie das Swiss Cheese von Paul?
 # MNAR 
 data_mnar <- data_orig
 z_miss_mnar_p <-
-  0.5 + 1 * x1 - 0.7 * x2 - 5 * y + rnorm(n, 0, 3)
+  0.5 + 1 * X1 - 0.7 * X2 - 5 * y + rnorm(n, 0, 3)
 mis_mnar_p <- z_miss_mnar_p < quantile(z_miss_mnar_p, 0.1)
 data_mnar$y[mis_mnar_p] <- NA
 summary(data_mnar$y)
