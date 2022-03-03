4f167d357157722758958d08cacceb1ad1b2d71b
TobiasMeyerhoefer <97233541+TobiasMeyerhoefer@users.noreply.github.com>
2022-03-03 11:46:34 +0100

    Add files via upload

--- Convergence.R
+++ Convergence.R
@@ -1,4 +1,4 @@
-## Convergence of chained equation algorithms
+### Convergence of chained equation algorithms
 
 rm(list=ls())
 
@@ -10,22 +10,22 @@ library(mi)
 library(miceadds)
 
 
-#Listen für Rhat-Schätzer
+#Lists for Rhat-Estimator, that will be calculated in the for loop
 R_mean_30 = list(len=100) #mcar
 R_mean_70 = list(len=100)
 R_mean_mar_30 = list(len=100)
 R_mean_mar_70 = list(len=100)
-R_mean_my = list(len=100)
+R_mean_flex = list(len=100)
 R_var_30 = list(len=100)
 R_var_70 = list(len=100)
 R_var_mar_30 = list(len=100)
 R_var_mar_70 = list(len=100)
-R_var_my = list(len=100)
+R_var_flex = list(len=100)
 R_Rsq_30 = list(len=100)
 R_Rsq_70 = list(len=100)
 R_Rsq_mar_30 = list(len=100)
 R_Rsq_mar_70 = list(len=100)
-R_Rsq_my = list(len=100)
+R_Rsq_flex = list(len=100)
 
 # The following function belongs to the source code of the Rhat.mice function of the 
 # miceadds package and will be used later on in our code. See:
@@ -61,12 +61,12 @@ mat1 <- matrix(nrow=5, ncol=5)
 rhat_sq <- vector(length=5) # temporary saved value of rhat for R²
 
 
-#Reproduzierbarkeit
+#Reproducibility
 set.seed(1)
 
-# Data generation
+##### Data generation #####
 
-for (i in 1:2){
+for (i in 1:100){
   
   n <- 1000
   number_variables <- 6
@@ -81,15 +81,10 @@ for (i in 1:2){
   X5 <- 6 + 0.1 * y - 1.3 * X1 + 0.75 * X2 - 0.2 * X3 + 0.8 * X4 + rnorm(n, mu[6], v[6])
   data_orig <- data.frame(y, X1, X2, X3, X4, X5)
   
-  ## original values
-  #(means_orig <- apply(data_orig, 2, FUN = mean))
-  #(vars_orig <- apply(data_orig, 2, FUN = var))
-  #(cov_orig <- cov(data_orig))
-  #(cor_orig <- cor(data_orig))
   
-  # create missing values
-  
-  ## mcar
+  ##### mcar #####
+  ##create missing values
+  #we test two missing proportions, 30% and 70%
   for (percent_miss in c(0.3, 0.7)){
     data_mcar <- data_orig
     
@@ -104,28 +99,16 @@ for (i in 1:2){
     is.na(data_mcar$X2[mcar_values3]) <- T
     is.na(data_mcar$X3[mcar_values4]) <- T
     is.na(data_mcar$X4[mcar_values5]) <- T
-    #x5 bleibt vollständig
-    ## graphics of missing pattern
-    
-    #aggr(data_mcar, numbers = T, prop = F)
-    #md.pattern(data_mcar, plot = T)
-    
-    #matrixplot(data_mcar, sortby = "x")
-    #matrixplot(data_mcar, sortby = "X1")
-    #matrixplot(data_mcar, sortby = "X2")
-    
-    #marginplot(data_mcar[, c("y", "X1")])
-    #marginplot(data_mcar[, c("y", "X2")])
-    #marginplot(data_mcar[, c("X1", "X2")])
-    
-    ## mar
-    
-    #remove the lowest quantile
+    #x5 stays complete
+
+    ##### mar #####
+    ##create missing values
     data_mar <- data_orig
+    #numbers for dependencies were chosen randomly
     z_miss_mar_py <- 0.5 + 2 * X1 - 0.7 * X2 + 0.4 * X3 - 0.8 * X4 + 0.1 * X5 + rnorm(n, 0, 3)
     mis_mar_py <- z_miss_mar_py < quantile(z_miss_mar_py, percent_miss)
     
-    #für alle Variablen außer x5 durchführen
+    #x5 stays complete
     z_miss_mar_pX1 <- 0.3 + 2.5 * y - 0.5 * X2 + 0.6 * X3 - 0.9 * X4 + 0.2 * X5 + rnorm(n, 0, 3)
     mis_mar_pX1 <- z_miss_mar_pX1 < quantile(z_miss_mar_pX1, percent_miss)
     
@@ -138,51 +121,36 @@ for (i in 1:2){
     z_miss_mar_pX4 <- 0.4 + 0.9 * y + 0.6 * X1 + 0.7 * X2 - 0.9 * X3 + 0.3 * X5 + rnorm(n, 0, 3)
     mis_mar_pX4 <- z_miss_mar_pX4 < quantile(z_miss_mar_pX4, percent_miss)
     
-    #NA setzen der Beobachtungen
+    #setting the lowest quantile NA
     data_mar$y[mis_mar_py] <- NA
     data_mar$X1[mis_mar_pX1] <- NA
     data_mar$X2[mis_mar_pX2] <- NA
     data_mar$X3[mis_mar_pX3] <- NA
     data_mar$X4[mis_mar_pX4] <- NA
-    #Muster mal ansehen
-    #Anteile überprüfen, ob 0.3 hinkommt.
-    #summary(data_mar$y)
-    #summary(data_orig$y)
+
     
+    ##### Imputation #####
     
-    # Imputation
-    
-    ## mice
-    #mice mcar
+    #we use the mice package
+    #Imputation for MCAR
     mice_mcar <- mice(data_mcar, maxit=5)
-    summary(mice_mcar)
-    
-    #Funktioniert nicht
-    #means_mcar <- mice_mcar %>% complete() %>% colMeans()
-    #vars_mcar <- apply(mice_mcar %>% complete(), 2, FUN = var)
-    #cov_mcar <- mice_mcar %>% complete() %>% cov()
-    #cor_mcar <- mice_mcar %>% complete() %>% cor(., use = "pairwise.complete.obs")
-    
-    # This returns the difference of these values. As there are multiple numbers,
-    # I only return the highest, cause it is the most problematic (or is it? 
-    # They might be dependent on the original variance of the variables)
-    #max(means_mcar - means_orig)
-    #max(vars_mcar - vars_orig)
-    #max(cov_mcar - cov_orig)
-    #max(abs(cor_mcar) - abs(cor_orig))
-    
-    #mice mar
+   
+    #Imputation for MAR
     mice_mar <- mice(data_mar, maxit=5)
-    summary(mice_mar)
-    
-    #means_mar <- mice_mar %>% complete() %>% colMeans()
-    #vars_mar <- apply(mice_mar %>% complete(), 2, FUN = var)
-    #cov_mar <- mice_mar %>% complete() %>% cov()
-    #cor_mar <- mice_mar %>% complete() %>% cor(., use = "pairwise.complete.obs")
     
     
-    ##########################################################################
-    ####################### Rhat for R² ######################################
+    ##### Rhat for R² #####
+    #For each iteration and model (m) we need the imputed dataset. 
+    #Therefore maxit is first set to 1 (then 2, ... until 5). 
+    #For each iteration the default amount of models (m=5) will be created. 
+    #For the 5x5=25 datasets a linear model is calculated and the 
+    #corresponding R² is extracted in the sub-loop. 
+    #The result is a 5x5 matrix of R²s (mat1). 
+    #Now the function presented in the beginning of the script Rhat1 is used 
+    #to get the Rhat-value of this matrix, which is then saved as rhat_sq.
+    #The same loop is created to build a linear model for y, X1, X2, X3 and 
+    #X4 as the dependent variable.
+
     for (impute_data in c(data_mcar, data_mar)){
       
       
@@ -251,11 +219,11 @@ for (i in 1:2){
       }
       rhat_sq[5] <- Rhat1(mat1)
       
-      # Rhat
+      ##### Rhat for mean and variance from package miceadds #####
       if (percent_miss == 0.3){
-        R_mean_30[i] = Rhat.mice(mice_mcar)[3]
-        R_var_30[i] = Rhat.mice(mice_mcar)[4]
-        R_Rsq_30[[i]] = rhat_sq
+        R_mean_30[i] = Rhat.mice(mice_mcar)[3] #col 3 is the Rhat of the mean
+        R_var_30[i] = Rhat.mice(mice_mcar)[4] #col 4 is the Rhat of the variance
+        R_Rsq_30[[i]] = rhat_sq #saving the calculated Rhats for R²
       }
       if (percent_miss == 0.7){
         R_mean_70[i] = Rhat.mice(mice_mcar)[3]
@@ -274,32 +242,42 @@ for (i in 1:2){
       }
     }
   }
-  #Hr. Meinfelders Muster
-  data_my <- data_orig
-  #Da die Daten auf Zufallszügen basieren, ist hier kein richtiges Sampling notwendig
-  #10% der Daten als CC
-  #Gleiche große Anzahl an Missings in den Variablen, nicht realisisch aber ist es problematisch?
-  #Funktioniert das so? Ansonsten wie in Zeilen 102-106 anpassen
-  is.na(data_my$y[1:540]) <- T
-  is.na(data_my$X1[1:540]) <- T
-  is.na(data_my$X2[1:540]) <- T
-  is.na(data_my$X3[541:900]) <- T
-  is.na(data_my$X4[541:900]) <- T
-  
-  #mice "my"
-  mice_my <- mice(data_my, maxit=5)
-  summary(mice_my)
-  
-  R_mean_my[i] <-  Rhat.mice(mice_my)[3]
-  R_var_my[i] <-  Rhat.mice(mice_my)[4]
   
   
+  ##### Flex Pattern #####
   
-  #############################################################################
-  ############################# Rhat of R² ####################################
+  #When different questionnaires are given to two groups of people
+  #with only a few overlapping questions (e.g. demographics),
+  #there will be a missing pattern by design. 
+  #This pattern will be simulated here.
+  
+  data_flex <- data_orig
+  
+  #Since the order of the observations is random, no sampling is necessary.
+  #We build two groups: Group 1 is observed for y, X1 and X2 
+  #and group 2 is observed for X3 and X4.
+  #X5 is observed in both groups.
+  #Only 10 % of the data are complete cases. 
+  
+  is.na(data_flex$y[1:540]) <- T
+  is.na(data_flex$X1[1:540]) <- T
+  is.na(data_flex$X2[1:540]) <- T
+  is.na(data_flex$X3[541:900]) <- T
+  is.na(data_flex$X4[541:900]) <- T
+  
+  #Imputation for Flex Pattern
+  mice_flex <- mice(data_flex, maxit=5)
+  
+  R_mean_flex[i] <-  Rhat.mice(mice_flex)[3]
+  R_var_flex[i] <-  Rhat.mice(mice_flex)[4]
+  
+  # Rhat for R²
+  #The same code as for MCAR and MAR is used but we have to repeat it here
+  #because this pattern does not differentiate between 30 % and 70 %
+  
   # for the y variable as depvar
   for (iter in 1:5){
-    mice_iter <- mice(data_my, maxit=iter) # simulate the first iteration
+    mice_iter <- mice(data_flex, maxit=iter) # simulate the first iteration
     models <- lapply(1:mice_iter$m, function(m){
       lm(y ~ .,
          data = mice::complete(mice_iter, action = m))}) # action iterates between the m
@@ -312,7 +290,7 @@ for (i in 1:2){
   
   # for variable x1 as depvar
   for (iter in 1:5){
-    mice_iter <- mice(data_my, maxit=iter) # simulate the first iteration
+    mice_iter <- mice(data_flex, maxit=iter) # simulate the first iteration
     models <- lapply(1:mice_iter$m, function(m){
       lm(X1 ~ .,
          data = mice::complete(mice_iter, action = m))}) # action iterates between the m
@@ -325,7 +303,7 @@ for (i in 1:2){
   
   # for variable x2 as depvar
   for (iter in 1:5){
-    mice_iter <- mice(data_my, maxit=iter) # simulate the first iteration
+    mice_iter <- mice(data_flex, maxit=iter) # simulate the first iteration
     models <- lapply(1:mice_iter$m, function(m){
       lm(X2 ~ .,
          data = mice::complete(mice_iter, action = m))}) # action iterates between the m
@@ -338,7 +316,7 @@ for (i in 1:2){
   
   # for variable x3 as depvar
   for (iter in 1:5){
-    mice_iter <- mice(data_my, maxit=iter) # simulate the first iteration
+    mice_iter <- mice(data_flex, maxit=iter) # simulate the first iteration
     models <- lapply(1:mice_iter$m, function(m){
       lm(X3 ~ .,
          data = mice::complete(mice_iter, action = m))}) # action iterates between the m
@@ -351,7 +329,7 @@ for (i in 1:2){
   
   # for variable x4 as depvar
   for (iter in 1:5){
-    mice_iter <- mice(data_my, maxit=iter) # simulate the first iteration
+    mice_iter <- mice(data_flex, maxit=iter) # simulate the first iteration
     models <- lapply(1:mice_iter$m, function(m){
       lm(X4 ~ .,
          data = mice::complete(mice_iter, action = m))}) # action iterates between the m
@@ -361,10 +339,11 @@ for (i in 1:2){
     }
   }
   rhat_sq[5] <- Rhat1(mat1)
-  R_Rsq_my[[i]] <-  rhat_sq
-  
+  R_Rsq_flex[[i]] <-  rhat_sq
   
 }
+
+##### Calculate the share of converged estimators #####
 
 # This returns the share of the variables that have converged (have an Rhat<1.1)
 # It is calculated by taking the absolute number of converged variables and 
@@ -372,7 +351,7 @@ for (i in 1:2){
 # i = the amount of loops/ the amount of generated datasets that are tested
 # 5 = the 5 variables that have missing values 
 
-#Anteil an konvergierten Werten
+#MCAR
 con_mean_mcar_30 <- (lapply(R_mean_30,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
 con_mean_mcar_70 <- (lapply(R_mean_70,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
 con_var_mcar_30 <- (lapply(R_var_30,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
@@ -380,13 +359,7 @@ con_var_mcar_70 <- (lapply(R_var_70,function(x) x[which(x<1.1)]) %>%  unlist() %
 con_Rsq_mcar_30 <- (lapply(R_Rsq_30,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
 con_Rsq_mcar_70 <- (lapply(R_Rsq_70,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
 
-# how many variances have converges (6 variables x 100 iterations)
-
-
-#show the values that are under 1.1
-print(as.data.frame(t(lapply(R_mean_30,function(x) x[which(x<1.1)]))))
-
-###MAR konvergierte Anteile
+#MAR
 con_mean_mar_30 <- (lapply(R_mean_mar_30,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
 con_mean_mar_70 <-(lapply(R_mean_mar_70,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
 con_var_mar_30 <-(lapply(R_var_mar_30,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
@@ -394,22 +367,23 @@ con_var_mar_70 <-(lapply(R_var_mar_70,function(x) x[which(x<1.1)]) %>%  unlist()
 con_Rsq_mar_30 <-(lapply(R_Rsq_mar_30,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
 con_Rsq_mar_70 <-(lapply(R_Rsq_mar_70,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
 
-###Hr. Meinfelders Muster konvergierte Anteile
-con_mean_my <- (lapply(R_mean_my,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
-con_var_my <- (lapply(R_var_my,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
-con_Rsq_my <- (lapply(R_Rsq_my,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
+#Flex Pattern
+con_mean_flex <- (lapply(R_mean_flex,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
+con_var_flex <- (lapply(R_var_flex,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
+con_Rsq_flex <- (lapply(R_Rsq_flex,function(x) x[which(x<1.1)]) %>%  unlist() %>% length())/(i*5)
 
-###Collect data for sample
-missmech <- c(rep("MCAR30", 3), rep("MCAR70", 3), rep("MAR30", 3), rep("MAR70", 3), rep("MY_PATTERN", 3) )
-est <- rep(c("Mean" , "Variance" , "R^2"), 5)
-prop <- c(con_mean_mcar_30, con_var_mcar_30, con_Rsq_mcar_30, con_mean_mcar_70, con_var_mcar_70, con_Rsq_mcar_70, con_mean_mar_30, con_var_mar_30, con_Rsq_mar_30, con_mean_mar_70, con_var_mar_70, con_Rsq_mar_70, con_mean_my, con_var_my, con_Rsq_my)
+##### Graphical representation #####
+#Collect data for graph
+#Make a data frame from the share of the converged estimators calculated before
+missmech <- c(rep("MCAR30", 3), rep("MCAR70", 3), rep("MAR30", 3), rep("MAR70", 3), rep("Flex-Pattern", 3) )
+est <- rep(c("Mean" , "Variance" , "R²"), 5)
+prop <- c(con_mean_mcar_30, con_var_mcar_30, con_Rsq_mcar_30, con_mean_mcar_70, con_var_mcar_70, con_Rsq_mcar_70, con_mean_mar_30, con_var_mar_30, con_Rsq_mar_30, con_mean_mar_70, con_var_mar_70, con_Rsq_mar_70, con_mean_flex, con_var_flex, con_Rsq_flex)
 plotdata <- data.frame(missmech, est, prop)
 
 # Plot
 ggplot(plotdata, aes(fill=est, y=prop, x=missmech)) + 
   geom_bar(position="dodge", stat="identity") +
-  xlab("Missing Mechanisms") +
-  ylab("Proportion of Converged Estimators") +
-  scale_fill_discrete(name= "Estimators") +
-  ggtitle("Proportions of Converged Estimators by Pattern") +
+  xlab("Missing Mechanisms & Pattern") +
+  ylab("Share of Converged Estimators") +
+  scale_fill_manual(name="Estimators", values = c("Mean" = "lightblue", "R²" = "lightcoral", "Variance" = "grey")) +
   theme_bw()
